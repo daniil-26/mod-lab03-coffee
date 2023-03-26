08365348f574a4b8e2ef19118bce01255d16d336
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-26 23:50:27 +0300

    Update Automata.cpp

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,130 +1,131 @@
+// Copyright 2022 UNN-IASR
 #include "Automata.h"
 #include <iostream>
 
 
 Automata::Automata()
 {
-	cash = 0;
-	state = OFF;
+    cash = 0;
+    state = OFF;
 }
 
 void Automata::on()
 {
-	if (state == OFF)
-	{
-		cash = 0;
-		state = WAIT;
-		getMenu();
-	}
+    if (state == OFF)
+    {
+        cash = 0;
+        state = WAIT;
+        getMenu();
+    }
 }
 
 void Automata::off()
 {
-	getChange();
-	cash = 0;
-	state = OFF;
+    getChange();
+    cash = 0;
+    state = OFF;
 }
 
 void Automata::coin(unsigned int sum)
 {
-	if (state == WAIT || state == ACCEPT)
-	{
-		cash += sum;
-		state = ACCEPT;
-	}
-	else
-	{
-		cout << "Operation cannot be performed" << endl;
-	}
+    if (state == WAIT || state == ACCEPT)
+    {
+        cash += sum;
+        state = ACCEPT;
+    }
+    else
+    {
+        cout << "Operation cannot be performed" << endl;
+    }
 }
 
 void Automata::getMenu()
 {
-	cout << "Menu:" << endl;
-	for (int i = 0; i < sizeof(menu) / sizeof(menu[0]); i++)
-	{
-		cout << i + 1 << ") " << menu[i] << " -  " << prices[i] << endl;
-	}
+    cout << "Menu:" << endl;
+    for (int i = 0; i < sizeof(menu) / sizeof(menu[0]); i++)
+    {
+        cout << i + 1 << ") " << menu[i] << " -  " << prices[i] << endl;
+    }
 }
 
 STATES Automata::getState()
 {
-	return state;
+    return state;
 }
 
 void Automata::choice(unsigned int num)
 {
-	if (state == ACCEPT)
-	{
-		if (0 < num && num <= sizeof(menu) / sizeof(menu[0]))
-		{
-			num_of_chosen_drink = num - 1;
-			state = CHECK;
-			check();
-		}
-		else
-		{
-			cout << "Incorrect choice" << endl;
-		}
-	}
-	else
-	{
-		cout << "Operation cannot be performed" << endl;
-	}
+    if (state == ACCEPT)
+    {
+        if (0 < num && num <= sizeof(menu) / sizeof(menu[0]))
+        {
+            num_of_chosen_drink = num - 1;
+            state = CHECK;
+            check();
+        }
+        else
+        {
+            cout << "Incorrect choice" << endl;
+        }
+    }
+    else
+    {
+    cout << "Operation cannot be performed" << endl;
+    }
 }
 
 void Automata::cancel()
 {
-	if (state == ACCEPT || state == CHECK)
-	{
-		getChange();
-		cash = 0;
-		state = WAIT;
-	}
-	else
-	{
-		cout << "Operation cannot be performed" << endl;
-	}
+    if (state == ACCEPT || state == CHECK)
+    {
+        getChange();
+        cash = 0;
+        state = WAIT;
+    }
+    else
+    {
+        cout << "Operation cannot be performed" << endl;
+    }
 }
 
 void Automata::check()
 {
-	if (state == CHECK)
-	{
-		if (cash >= prices[num_of_chosen_drink])
-		{
-			getChange(prices[num_of_chosen_drink]);
-			cook();
-		}
-		else
-		{
-			cout << "Insufficient funds" << endl;
-			cancel();
-		}
-	}
+    if (state == CHECK)
+    {
+        if (cash >= prices[num_of_chosen_drink])
+        {
+            getChange(prices[num_of_chosen_drink]);
+            cook();
+        }
+        else
+        {
+            cout << "Insufficient funds" << endl;
+            cancel();
+        }
+    }
 }
 
 void Automata::cook()
 {
-	if (state == CHECK)
-	{
-		state = COOK;
-		cout << "Your " << menu[num_of_chosen_drink] << " is ready" << endl;
-		finish();
-	}
+    if (state == CHECK)
+    {
+        state = COOK;
+        cout << "Your " << menu[num_of_chosen_drink] << " is ready" << endl;
+        finish();
+    }
 }
 
 void Automata::finish()
 {
-	if (state == COOK)
-	{
-		cash = 0;
-		state = WAIT;
-	}
+    if (state == COOK)
+    {
+        cash = 0;
+        state = WAIT;
+    }
 }
 
 void Automata::getChange(unsigned int price)
 {
-	if (cash - price != 0)
-		cout << "Take the change: " << cash - price << endl;
+    if (cash - price != 0)
+    cout << "Take the change: " << cash - price << endl;
 }
