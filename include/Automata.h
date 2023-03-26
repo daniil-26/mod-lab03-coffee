570e940cfd6ca47c34150888a8a15d279e9d3bd1
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-27 00:20:51 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -7,11 +7,11 @@ enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
 class Automata {
  private:
     unsigned int cash;
-    std::string menu[6] = {"americano", 
-                           "cappuccino", 
-                           "latte", 
-                           "espresso", 
-                           "hot chocolate", 
+    std::string menu[6] = {"americano",
+                           "cappuccino",
+                           "latte",
+                           "espresso",
+                           "hot chocolate",
                            "tea"};
     unsigned int prices[6] = {100, 90, 80, 90, 80, 40};
     STATES state;
