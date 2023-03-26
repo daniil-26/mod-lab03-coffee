16dfc4995080a33bf00223e838b708db4883c76e
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2023-03-26 23:15:06 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,37 @@
+#include <string>
+using namespace std;
+
+
+enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
+
+class Automata
+{
+
+private:
+
+	unsigned int cash;
+	string menu[6] = {"americano", "cappuccino", "latte", "espresso", "hot chocolate", "tea"};
+	unsigned int prices[6] = {100, 90, 80, 90, 80, 40};
+	STATES state;
+	unsigned int num_of_chosen_drink;
+
+
+public:
+
+	Automata();
+	void on();
+	void off();
+	void coin(unsigned int sum);
+	void getMenu();
+	STATES getState();
+	void choice(unsigned int num);
+	void cancel();
+
+
+private:
+
+	void check();
+	void cook();
+	void finish();
+	void getChange(unsigned int price = 0);
+};
